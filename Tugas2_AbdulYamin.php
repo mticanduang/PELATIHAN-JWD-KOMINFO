3c06d2f2fb4b71d3f4e80d8bb05baad5a65c99c1
MTI CANDUANG <61419900+mticanduang@users.noreply.github.com>
2021-09-27 12:43:06 +0700

    Tugas 2 - Mengeksekusi source code
    Tugas 2 - Mengeksekusi source code

---
+++ Tugas2_AbdulYamin.php
@@ -0,0 +1,13 @@
+<!-- tag pembuka PHP -->
+<?php
+// Author : Abdul Yamin
+// Title  : Tugas 2 - Mengeksekusi source code 
+
+// Menampilkan text/string berikut ini :
+$txt_tempat = "BPPTIK Cikarang";
+echo "Selamat Datang <br>";
+echo "Ayo Kita Belajar Junior Web Developer bersama <br>";
+echo "di".$txt_tempat;
+
+?>
+<!-- tag penutup php -->
